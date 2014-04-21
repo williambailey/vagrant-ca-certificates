b78f7d2fc38ea2e184425aef62398ac37f805afe
John Bellone <jbellone@bloomberg.net>
2014-04-21 16:03:10 -0400

    Add capability for URL objects to be passed through.

--- lib/vagrant-ca-certificates/config/ca_certificates.rb
+++ lib/vagrant-ca-certificates/config/ca_certificates.rb
@@ -1,4 +1,6 @@
 require 'vagrant'
+require 'vagrant/util/downloader'
+require 'tempfile'
 
 module VagrantPlugins
   module CaCertificates
@@ -18,6 +20,13 @@ module VagrantPlugins
           @enabled = false if @enabled == UNSET_VALUE
           @certs = [] if @certs == UNSET_VALUE
           @certs_path = '/usr/share/ca-certificates' if @certs_path == UNSET_VALUE
+
+          @certs.each do |cert|
+            next unless cert.is_a?(URL)
+            tempfile = Tempfile.new(['cacert', '.pem'])
+            Vagrant::Util::Downloader.new(cert.to_s, tempfile.path)
+            cert = tempfile.path
+          end
         end
       end
     end
