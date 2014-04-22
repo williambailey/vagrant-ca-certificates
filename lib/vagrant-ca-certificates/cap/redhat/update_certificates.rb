2aa25b47789fc3a0688574911bdcbc0dc6c256be
William Bailey <wbailey4@bloomberg.net>
2014-04-22 12:45:16 +0100

    Add comment to redhat update certificate action to say thats its not currently idempotent.

--- lib/vagrant-ca-certificates/cap/redhat/update_certificates.rb
+++ lib/vagrant-ca-certificates/cap/redhat/update_certificates.rb
@@ -10,6 +10,13 @@ module VagrantPlugins
 
             # Assume that all of the certificates have been uploaded and just concatenate
             # them to the proper certificate bundle in /etc/pki/tls/cacerts.
+            #
+            # TODO: make idempotent.
+            #
+            # Currently this will not remove any previously installed vagrant certificate and
+            # will append a copy of the uploaded certificates each time provision is run. If
+            # you run provision multiple time then you will see the certificates duplicated
+            # in the ca-cundle.crt file.
             machine.communicate.sudo("find #{ca_certs} -maxdepth 1 -type f -exec cat {} >> #{ca_bundle} \\;")
           end
         end
